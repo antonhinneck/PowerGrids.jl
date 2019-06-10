2288d04276eada5f94f7d22abab7e68dc5685aef
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-10 17:27:05 +0300

    version 0.2

--- test/export.jl
+++ test/export.jl
@@ -1,6 +1,5 @@
 include("C:/Users/Anton Hinneck/.julia/packages/GraphVisualization/src/GraphVisualization.jl")
 include("C:/Users/Anton Hinneck/.julia/packages/PowerGrids/src/PowerGrids.jl")
 
-grid = PowerGrids.readDataset(PowerGrids.datasets()[6])
-
+grid = PowerGrids.readDataset(PowerGrids.datasets()[5])
 graph = PowerGrids.toGraph(grid)
