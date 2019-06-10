73462e9e054e5f2d9fed44d41fcc21e9c9571c55
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-10 13:26:31 +0300

    version 0.2

--- src/IO/io_graph.jl
+++ src/IO/io_graph.jl
@@ -11,9 +11,17 @@ function toGraph(PG::PowerGrid)
     # 3: Demand
     VertexTypeLabels = Dict{I where I <: Integer, String}()
 
+    EdgeLabels = Dict{Tuple{I where I <: Integer, I where I <: Integer}, String}()
+    EdgeTypes = Dict{Tuple{I where I <: Integer, I where I <: Integer}, I where I <: Integer}()
+    # 1: Power Line
+    # 2: Connection (Generator - Bus)
+    EdgeTypeLabels = Dict{I where I <: Integer, String}()
+
     vertices = 0
     for i in 1:length(PG.busses)
 
+        # Add level 1 nodes
+        #------------------
         add_vertex!(graph)
         vertices += 1
         push!(VertexLabels, vertices => string(PG.busses[i]))
@@ -22,18 +30,38 @@ function toGraph(PG::PowerGrid)
 
         for j in 1:length(PG.generators_at_bus[i])
 
+            # Add level 2 nodes
+            #------------------
             add_vertex!(graph)
             vertices += 1
             push!(VertexLabels, vertices => string("gen: ", PG.generators_at_bus[i][j]))
             push!(VertexTypes, vertices => 2)
             push!(VertexTypeLabels, vertices => "generator")
 
+            # Add level 2 edges
+            #------------------
+            current_edge = Tuple([i,j])
+            add_edge!(graph, current_edge...)
+            push!(EdgeLabels, current_edge => string("e: "current_edge[1]," - ",current_edge[2]))
+            push!(EdgeTypes, current_edge => 2)
+            push!(EdgeTypeLabels, current_edge => "connection")
+
         end
     end
 
-    EdgeLabels = Dict{Tuple{I where I <: Integer, I where I <: Integer}, String}()
-    EdgeTypes = Dict{Tuple{I where I <: Integer, I where I <: Integer}, I where I <: Integer}()
-    EdgeTypeLabels = Dict{I where I <: Integer, String}()
+    for i in 1:length(PG.lines)
+
+        # Add level 1 edges
+        #------------------
+        current_edge = Tuple([PG.line_start[lines[i]], PG.line_end[lines[i]]])
+        add_edge!(graph, current_edge...)
+        push!(EdgeLabels, current_edge => string("e: "current_edge[1]," - ",current_edge[2]))
+        push!(EdgeTypes, current_edge => 1)
+        push!(EdgeTypeLabels, current_edge => "power_line")
+
+    end
+
+    for i in 1:length(generator)
 
     return GraphVisualization.AnnotatedSimpleGraph(graph,
                                                     VertexLabels,
