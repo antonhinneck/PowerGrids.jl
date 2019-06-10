4ee2cfb4b4e25b6a87d69fc2664b230ae7fb3ab6
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-10 15:11:31 +0300

    version 0.2

--- src/IO/io_graph.jl
+++ src/IO/io_graph.jl
@@ -10,12 +10,17 @@ function toGraph(PG::PowerGrid)
     # 2: Generator
     # 3: Demand
     VertexTypeLabels = Dict{I where I <: Integer, String}()
+    push!(VertexTypeLabels, 1 => "bus")
+    push!(VertexTypeLabels, 2 => "generator")
+    push!(VertexTypeLabels, 3 => "demand")
 
     EdgeLabels = Dict{Tuple{I where I <: Integer, I where I <: Integer}, String}()
     EdgeTypes = Dict{Tuple{I where I <: Integer, I where I <: Integer}, I where I <: Integer}()
     # 1: Power Line
     # 2: Connection (Generator - Bus)
     EdgeTypeLabels = Dict{I where I <: Integer, String}()
+    push!(EdgeTypeLabels, 1 => "power_line")
+    push!(EdgeTypeLabels, 2 => "connnection")
 
     vertices = 0
     for i in 1:length(PG.busses)
@@ -24,27 +29,26 @@ function toGraph(PG::PowerGrid)
         #------------------
         add_vertex!(graph)
         vertices += 1
+        top_level_vertex = vertices
         push!(VertexLabels, vertices => string(PG.busses[i]))
         push!(VertexTypes, vertices => 1)
-        push!(VertexTypeLabels, vertices => "bus")
 
-        for j in 1:length(PG.generators_at_bus[i])
-
+        for j in 1:length(PG.generators_at_bus[PG.busses[i]])
+            #print(top_level_vertex," - ")
             # Add level 2 nodes
             #------------------
             add_vertex!(graph)
             vertices += 1
             push!(VertexLabels, vertices => string("gen: ", PG.generators_at_bus[i][j]))
             push!(VertexTypes, vertices => 2)
-            push!(VertexTypeLabels, vertices => "generator")
 
             # Add level 2 edges
             #------------------
-            current_edge = Tuple([i,j])
+            current_edge = Tuple([top_level_vertex, vertices])
+            print(current_edge," - ")
             add_edge!(graph, current_edge...)
-            push!(EdgeLabels, current_edge => string("e: "current_edge[1]," - ",current_edge[2]))
+            push!(EdgeLabels, current_edge => string("e: ",current_edge[1]," - ",current_edge[2]))
             push!(EdgeTypes, current_edge => 2)
-            push!(EdgeTypeLabels, current_edge => "connection")
 
         end
     end
@@ -53,15 +57,12 @@ function toGraph(PG::PowerGrid)
 
         # Add level 1 edges
         #------------------
-        current_edge = Tuple([PG.line_start[lines[i]], PG.line_end[lines[i]]])
+        current_edge = Tuple([PG.line_start[PG.lines[i]], PG.line_end[PG.lines[i]]])
         add_edge!(graph, current_edge...)
-        push!(EdgeLabels, current_edge => string("e: "current_edge[1]," - ",current_edge[2]))
+        push!(EdgeLabels, current_edge => string("e: ",current_edge[1]," - ",current_edge[2]))
         push!(EdgeTypes, current_edge => 1)
-        push!(EdgeTypeLabels, current_edge => "power_line")
 
     end
-
-    for i in 1:length(generator)
 
     return GraphVisualization.AnnotatedSimpleGraph(graph,
                                                     VertexLabels,
