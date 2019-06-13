6a3dc8447cb5bc3975c913f57439cf138209604e
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-13 15:35:34 +0300

    version 0.2

--- test/export.jl
+++ test/export.jl
@@ -2,9 +2,9 @@ include("C:/Users/Anton Hinneck/.julia/packages/GraphVisualization/src/GraphVisu
 include("C:/Users/Anton Hinneck/.julia/packages/PowerGrids/src/PowerGrids.jl")
 
 #grid = PowerGrids.readDataset(PowerGrids.datasets()[5]) # 14 busses
-#grid = PowerGrids.readDataset(PowerGrids.datasets()[36]) # 588 busses
-grid = PowerGrids.readDataset(PowerGrids.datasets()[2]) # 10000 busses
+grid = PowerGrids.readDataset(PowerGrids.datasets()[36]) # 588 busses
+#grid = PowerGrids.readDataset(PowerGrids.datasets()[2]) # 10000 busses
 PowerGrids.datasets()
 graph = PowerGrids.toGraph(grid)
 
-GraphVisualization.plot(graph, [500,500])
+GraphVisualization.plot(graph, [900,900])
