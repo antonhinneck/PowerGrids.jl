87f7574303490fc163d5f4fa69a2216ff77336b0
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-20 16:25:25 +0300

    version 0.2

--- src/IO/io_json.jl
+++ src/IO/io_json.jl
@@ -9,3 +9,10 @@ mutable struct json_model
     generators::Vector{G where G <: Generator}
 
 end
+
+#postBody = JSON.json(data.jsonModel)
+#HTTP.request("POST", "http://localhost:8080/data/upload", ["Content-Type" => "application/json;charset=UTF-8"], postBody)
+
+#JSON.print(stdout, data.jsonModel, 2)
+
+#include("Model_switching.jl")
