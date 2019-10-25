e180937704188550e6ec0369d86c9fc9c933bc3c
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-10-25 17:14:05 +0300

    json interface: upload clustering

--- src/IO/io_json.jl
+++ src/IO/io_json.jl
@@ -24,6 +24,23 @@ mutable struct lmps_model
 
 end
 
+struct NodalClusterAssignment
+
+    bus_i::Int64
+    clusterNumber::Int64
+
+end
+
+mutable struct NodalClustering
+
+    datasetName::String
+    name::String
+    type::String
+    clusterAmount::Int64
+    nodalClusterAssignments::Vector{NodalClusterAssignment}
+
+end
+
 function upload_dataset(data, url::String)
 
     postBody = JSON.json(data.jsonModel)
@@ -42,6 +59,28 @@ function upload_lmps(name::String, _buses::Vector{Int64}, _lmps::Vector{Float64}
 
     postBody = JSON.json(lmps_model(name, lmps))
     HTTP.request("POST", url, ["Content-Type" => "application/json;charset=UTF-8"], postBody, require_ssl_verification = false)
+
+end
+
+function upload_clustering(data, _buses, _assignments, _url::String; name = "", type = "")
+#_name - denotes the cluster's name
+#_type - denotes the clustering method (e.g. "spatial")
+
+    _clusterAmount = length(unique(_assignments))
+
+    ncas = Vector{NodalClusterAssignment}()
+    for i in 1:length(_buses)
+        nca = NodalClusterAssignment(_buses[i], _assignments[i])
+        push!(ncas, nca)
+    end
+
+    nc = NodalClustering(data.jsonModel.name, name, type, _clusterAmount, ncas)
+    println(length(_assignments))
+    println(length(_buses))
+    println(length(ncas))
+
+    postBody = JSON.json(nc)
+    HTTP.request("POST", _url, ["Content-Type" => "application/json;charset=UTF-8"], postBody, require_ssl_verification = false)
 
 end
 
