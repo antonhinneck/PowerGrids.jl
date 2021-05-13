a6e35fb9f0eb0e75db831bf5ad22e43d9f7a1c43
Anton Hinneck <anton.hinneck@gmail.com>
2021-05-13 13:19:17 +0300

    param_mat_(A/B)

--- src/utils_datastructures.jl
+++ src/utils_datastructures.jl
@@ -9,12 +9,12 @@ function param_mat_A(pg::T where T <: PowerGrid)
     return _mat_A
 end
 
-function param_mat_B(pg::T where T <: PowerGrid)
+function param_mat_X(pg::T where T <: PowerGrid)
     # Returns B matrix including the susceptence 
     # of every line in pg on its diagonal.
-    _mat_B = zeros(size(pg.lines, 1), size(pg.lines, 1))
+    _mat_X = zeros(size(pg.lines, 1), size(pg.lines, 1))
     for i in 1:size(pg.lines, 1)
-        _mat_B[i, i] = pg.line_reactance[i]
+        _mat_X[i, i] = pg.line_reactance[i]
     end
-    return _mat_B
+    return _mat_X
 end
