ba853004b70034733878a0a97d37685a8caf18fb
Anton Hinneck <anton.hinneck@gmail.com>
2021-05-13 13:37:41 +0300

    param_mat_(A/B)

--- src/utils_datastructures.jl
+++ src/utils_datastructures.jl
@@ -1,3 +1,16 @@
+function param_vec_d(pg::T where T <: PowerGrid)
+    # Returns vector of bus loads.
+    _vec_d = zeros(size(pg.buses, 1))
+    for i in 1:size(pg.buses, 1)
+        try
+            _vec_d[i] = pg.bus_demand[i]
+        catch ex
+            _vec_d[i] = 0
+        end
+    end
+    return _vec_d
+end
+
 function param_mat_A(pg::T where T <: PowerGrid)
     # Returns matrix A, which contains 1 if line e
     # starts and -1 if line e ends at node v.
