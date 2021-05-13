720a00c73d2e962ebc484b702ffd3317fdf03de9
Anton Hinneck <anton.hinneck@gmail.com>
2021-05-13 13:05:31 +0300

    param_mat_(A/B)

--- src/utils_datastructures.jl
+++ src/utils_datastructures.jl
@@ -1,7 +1,7 @@
 function param_mat_A(pg::T where T <: PowerGrid)
     # Returns matrix A, which contains 1 if line e
     # starts and -1 if line e ends at node v.
-    _mat_A = zeros(size(pg.lines), size(pg.buses))
+    _mat_A = zeros(size(pg.lines, 1), size(pg.buses, 1))
     for i in 1:pg.lines
         _mat_A[i, pg.line_start[i]]
         _mat_A[i, pg.line_end[i]]
@@ -12,7 +12,7 @@ end
 function param_mat_B(pg::T where T <: PowerGrid)
     # Returns B matrix including the susceptence 
     # of every line in pg on its diagonal.
-    _mat_B = zeros(size(pg.lines), size(pg.lines))
+    _mat_B = zeros(size(pg.lines, 1), size(pg.lines, 1))
     for i in 1:pg.lines
         _mat_B[i, i] = pg.line_resistance[i]
     end
