2b304353ca5fc5162448c0389f322f9e71101b91
Anton Hinneck <anton.hinneck@gmail.com>
2021-05-13 14:51:08 +0300

    param_mat_(A/B)

--- src/PowerGrids.jl
+++ src/PowerGrids.jl
@@ -24,7 +24,7 @@ using LightGraphs: ne, nv, AbstractSimpleGraph, SimpleGraph, add_vertex!, add_ed
 export set_csv_path, csv_cases, select_csv_case, loadCase,
        __splitBus!, splitBus!, addBus!, addLine!, newBus!, update_line,
        split_build_x0, dfs_components, reassign_generator!, reassign_demand!, reduce_grid,
-       param_mat_A, param_mat_B, param_vec_d
+       param_mat_A, param_mat_B, param_vec_d, param_vec_x, param_vec_fmax, param_vec_Pmax
 
 # cases, readDataset, toGraph, AnnotatedSimpleGraph, paton, adjacency_list,
 #         SpanningTreeToIndicators, rndPermute, rmLines!, solutionToLineIndicators!, upload_dataset,
