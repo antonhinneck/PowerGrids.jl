b4aa48e8b11285aa25a33789b087df6dbfb86b5e
Anton Hinneck <anton.hinneck@gmail.com>
2021-05-13 14:32:06 +0300

    param_mat_(A/B)

--- src/utils_datastructures.jl
+++ src/utils_datastructures.jl
@@ -1,4 +1,20 @@
-function param_vec_d(pg::T where T <: PowerGrid)
+function param_vec_Pmax(pg::T where T <: PowerGrid; normalized = false)
+    # Returns vector of bus loads.
+    _vec_Pmax = zeros(size(pg.generators, 1))
+    for i in 1:size(pg.generators, 1)
+        try
+            _vec_Pmax[i] = pg.generator_capacity_max[i]
+        catch ex
+            _vec_Pmax[i] = 0
+        end
+    end
+    if normalized
+        _vec_Pmax /= pg.base_mva
+    end
+    return _vec_Pmax
+end
+
+function param_vec_d(pg::T where T <: PowerGrid; normalized = false)
     # Returns vector of bus loads.
     _vec_d = zeros(size(pg.buses, 1))
     for i in 1:size(pg.buses, 1)
@@ -8,10 +24,13 @@ function param_vec_d(pg::T where T <: PowerGrid)
             _vec_d[i] = 0
         end
     end
+    if normalized
+        _vec_d /= pg.base_mva
+    end
     return _vec_d
 end
 
-function param_mat_A(pg::T where T <: PowerGrid)
+function param_mat_A(pg::T where T <: PowerGrid; _feas_check = false)
     # Returns matrix A, which contains 1 if line e
     # starts and -1 if line e ends at node v.
     _mat_A = zeros(size(pg.lines, 1), size(pg.buses, 1))
@@ -19,6 +38,17 @@ function param_mat_A(pg::T where T <: PowerGrid)
         _mat_A[i, pg.line_start[i]] = 1
         _mat_A[i, pg.line_end[i]] = -1
     end
+    # Check feasibility of matrix, rows should sum to 0.
+    _feas = true
+    if _feas_check
+        for i in 1:size(_mat_A,1)
+            println(sum(_mat_A[i, :]))
+            if sum(_mat_A[i, :]) != 0
+                _feas = false
+            end
+        end
+    end
+    @assert feas == true "Rows do not sum to 1."
     return _mat_A
 end
 
