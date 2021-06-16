b29814830f50d82de7a802ae134e6600de89c9d7
Anton Hinneck <anton.hinneck@gmail.com>
2021-06-16 09:33:31 +0300

    changes

--- src/PowerGrids.jl
+++ src/PowerGrids.jl
@@ -1,13 +1,6 @@
 ## PowerGrids provides unified access
 ## to data sets like pglib in Julia.
 #####--------------------------------
-## For offline use, the package
-## contains a collection of pglib
-## data sets in xlsx format.
-#####--------------------------------
-## For online use, an interface
-## to opengrid.io is available.
-#####--------------------------------
 
 ## AUTHOR
 ## Anton Hinneck
@@ -25,7 +18,7 @@ export set_csv_path, csv_cases, select_csv_case, loadCase,
        __splitBus!, splitBus!, addBus!, addLine!, newBus!, update_line,
        split_build_x0, dfs_components, reassign_generator!, reassign_demand!, reduce_grid,
        param_mat_A, param_mat_B, param_vec_d, param_vec_x, param_vec_fmax, param_vec_Pmax,
-       param_vec_c0, param_vec_c1, param_vec_c2, param_mat_c2_rt, param_mat_P, toGraph
+       param_vec_c0, param_vec_c1, param_vec_c2, param_mat_c2_rt, param_mat_P, toGraph, sol_allActive
 
 # cases, readDataset, toGraph, AnnotatedSimpleGraph, paton, adjacency_list,
 #         SpanningTreeToIndicators, rndPermute, rmLines!, solutionToLineIndicators!, upload_dataset,
